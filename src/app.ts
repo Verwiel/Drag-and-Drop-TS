d334670768af2e3be6ae5531171c5c556072a788
unknown <verwiel.code@gmail.com>
2022-01-24 10:00:40 -0700

    reusable validation

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,33 @@
+// Validation
+interface Validatable {
+  value: string | number,
+  required?: boolean,
+  minLength?: number,
+  maxLength?: number,
+  minValue?: number,
+  maxValue?: number
+}
+
+function validate(validatableInput: Validatable) {
+  let isValid = true
+  if(validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0
+  }
+  if(validatableInput.minLength != null && typeof validatableInput.value === 'string') {
+    isValid = isValid && validatableInput.value.trim().length >= validatableInput.minLength
+  }
+  if(validatableInput.maxLength != null && typeof validatableInput.value === 'string') {
+    isValid = isValid && validatableInput.value.trim().length <= validatableInput.maxLength
+  }
+  if(validatableInput.minValue != null && typeof validatableInput.value === 'number') {
+    isValid = isValid && validatableInput.value >= validatableInput.minValue
+  }
+  if(validatableInput.maxValue != null && typeof validatableInput.value === 'number') {
+    isValid = isValid && validatableInput.value <= validatableInput.maxValue
+  }
+  return isValid
+}
+
 // autobind decorator
 function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
   const ogMethod = descriptor.value
@@ -37,10 +67,59 @@ class ProjectInput {
     this.attach()
   }
 
+  // use void if no value is returned since void is exclusive to functions
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputEl.value
+    const enteredDescription = this.descriptionInputEl.value
+    const enteredPeople = this.peopleInputEl.value
+
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true
+    }
+
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5
+    }
+
+    const peopleValidatable: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      minValue: 1,
+      maxValue: 5
+    }
+
+    if (
+      !validate(titleValidatable) || 
+      !validate(descriptionValidatable) || 
+      !validate(peopleValidatable)
+    ) {
+      alert('Invalid input, please try again!')
+      return
+    } else {
+      // use + to convert string to number, .value always returns string
+      return [enteredTitle, enteredDescription, +enteredPeople]
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputEl.value = ''
+    this.descriptionInputEl.value = ''
+    this.peopleInputEl.value = ''
+  }
+
   @AutoBind
   private submitHandler(event: Event) {
     event.preventDefault()
-    console.log(this.titleInputEl.value);
+    const userInput = this.gatherUserInput()
+    // since its a tuple, check if array so it works in JS
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput
+      console.log(title, description, people);
+      this.clearInputs()
+    }
   }
 
   private configure() {
